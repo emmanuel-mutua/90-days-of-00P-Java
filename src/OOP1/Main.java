08e30d5f6ce5a4555dc778027e021fb5f88579a0
Emmutua <emmamulwa2020@gmail.com>
2022-10-07 12:04:38 +0300

    strings

--- src/OOP1/Main.java
+++ src/OOP1/Main.java
@@ -5,3 +5,19 @@ public class Main {
 
     }
 }
+//METHODS
+/*public class methods {
+    //java methods can be overloaded, you can use the same names for a method but specify the parametes, one can have more parameters than the other
+    private static void addnums(int a,int b){
+        System.out.println("Sum: " + (a + b) + "\n");
+    }
+
+    private static void addnums(int j, int k,int m){
+        System.out.println("sum :" + (j+k+m));
+    }
+
+    public static void main(String[] args) {
+        addnums(1,3);
+        addnums(1, 4, 4);
+    }
+}*/
