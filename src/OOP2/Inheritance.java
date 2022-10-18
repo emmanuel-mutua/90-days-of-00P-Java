20f6068c2be3092967dda6243dd3ff148a0561a4
Emmutua <emmamulwa2020@gmail.com>
2022-10-18 14:54:25 +0300

    the super keyword

--- src/OOP2/Inheritance.java
+++ src/OOP2/Inheritance.java
@@ -1,26 +1,27 @@
 package OOP2;
 
+
 class Parent{
-    String name;
+    String nam;
     public void display(){
         System.out.println("This is the parent class");
     }
 }
-class Child extends parent {
+class Child extends Parent {
     public void display(){
-        System.out.println("This is the child class, it is inherited from the " + name);
+        System.out.println("This is the child class, it is inherited from the " + nam);
     }
 }
 
 public class Inheritance {
     public static void main (String[]args){
         Child emm = new Child();
-        parent par = new parent();
-        emm.name ="parent class";
+        Parent par = new Parent();
+        emm.nam ="parent class";
         emm.display();
 //        checking whether a class is an instance of the other class
         System.out.println(emm instanceof Child);
-        System.out.println(emm instanceof parent);
+        System.out.println(emm instanceof Parent);
     }
 }
 /**
--- src/OOP2/method_overriding.java
+++ src/OOP2/method_overriding.java
@@ -1,27 +1,38 @@
 package OOP2;
 //you cannot define a parent class as final
-class parent {
-    protected String name;
-    public void displayName(){
-        this.name = "Parent class";
-        System.out.println(name);
+abstract class par {
+    protected String name = "parent class";
+
+    public void displayNam() {
+
     }
+
+    public void showName() {
+
+    }
+
 }
- class child extends parent{
+ class child extends par{
     @Override
 //    the override specifies that the method overrides the method in the superclass
 //    it provides a different implementation of a method already defined in the parent class
 //    it is used when the child method has the same name with the parent class
 //    to provide a specific implementation of a method already done in the parent class
-    public void displayName(){
+    public void displayNam(){
         this.name = "Child class";
         System.out.println(name);
+        System.out.println("Child class is inherited from the " + super.name);
+    }
+    @Override
+    public void showName(){
+        System.out.println("I am the " + name);
+        System.out.println("I have been inherited from the " + super.name );
     }
 }
 public class method_overriding {
     public static void main(String[]args){
         child ob = new child();
-        ob.displayName();
+        ob.showName();
     }
 
 }
